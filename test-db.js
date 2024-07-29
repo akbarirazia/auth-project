0aa84a4d30e8477e92c957f7e45ad7c139e9f6e8
akbarirazia <akbariharis14@gmail.com>
2024-07-29 17:09:17 +0430

    first auth project created

---
+++ test-db.js
@@ -0,0 +1,15 @@
+const { Pool } = require("pg")
+require("dotenv").config()
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error("Error acquiring client", err.stack)
+  }
+  console.log("Connected to database successfully")
+  release()
+  pool.end()
+})
